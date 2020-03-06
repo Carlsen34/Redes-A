0aedfb59647ce8ca3760b2449212b1c643461b11
carlsen <herick.carlsen@gmail.com>
2020-03-06 10:39:09 -0300

    Adicionando no git os codigos exemplo postado pelo edmar

--- Atividade 3/TCP-Cliente-Basico.c
+++ Atividade 3/TCP-Cliente-Basico.c
@@ -7,174 +7,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 /*
  * Cliente TCP
  */
-
-#define MaxNAME 20
-#define MaxMsg 80
-#define MaxArray 10
-
-typedef struct {
-char Name[MaxNAME];
-char Msg[MaxMsg];
-int Opcao; //Informar ao servidor qual procedimento foi realizado
-} Obj; 
-
-
-char sendbuf[12];
-char recvbuf[200];
-int s;
-Obj objStore;
-
-
-// procedimento para apagar mensagem (opcao 3)
-void acessar_servidor_apagar_mensagem(Obj obj){
-    
-    // strcpy(sendbuf, obj.Name);
-
-
-    /* Envia a mensagem no buffer de envio para o servidor */
-    if (send(s, &obj, (sizeof(obj)), 0) < 0)
-    {
-        perror("Send()");
-        exit(5);
-    }
-    printf("\nMensagem enviada ao servidor\n");
-
-    /* Recebe a mensagem do servidor no buffer de recepcao */
-    if (recv(s, recvbuf, sizeof(recvbuf), 0) < 0)
-    {
-        perror("Recv()");
-        exit(6);
-    }
-    printf("Mensagem: %s\n", recvbuf);
-
-}
-
-
-// procedimento para enviar e receber mensagem do servidor
-void acessar_servidor(Obj obj){
-
-    // strcpy(sendbuf, obj.Name);
-
-
-    /* Envia a mensagem no buffer de envio para o servidor */
-    if (send(s, &obj, (sizeof(obj)), 0) < 0)
-    {
-        perror("Send()");
-        exit(5);
-    }
-    printf("\nMensagem enviada ao servidor\n");
-
-    /* Recebe a mensagem do servidor no buffer de recepcao */
-    if (recv(s, recvbuf, sizeof(recvbuf), 0) < 0)
-    {
-        perror("Recv()");
-        exit(6);
-    }
-    printf("%s\n", recvbuf);
-};
-
-// Procedimento para opcao 1 -  Cadastrar mensagem
-void adicionar_usuario_mensagens(){
-    Obj obj;
-    obj.Opcao = 1;
-    char name[MaxNAME];
-    char msg[MaxMsg];
-
-
-    printf("\nUsuario: \n");
-    __fpurge(stdin);
-    memset(name, 0, sizeof(name));
-    fgets(name,sizeof(name),stdin);
-    strtok(name,"\n");
-
-    printf("\nMensagem: \n");
-    __fpurge(stdin);
-    memset(msg, 0, sizeof(msg));
-    fgets(msg,sizeof(msg),stdin);
-    strtok(name,"\n");
-
-
-    strcpy(obj.Name,name);
-	strcpy(obj.Msg,msg);
-
-
-	acessar_servidor(obj);
-	// adicionar usuario e mensagens a ser cadastradas
-};
-
-void printMessages(int sizeOfObjStore) {
-
-    for(int i = 0; i < sizeOfObjStore; i++) {
-        memset(&objStore, 0, sizeof(objStore));
-        if (recv(s, &objStore, sizeof(objStore), 0) < 0)
-        {
-            perror("Recv()");
-            exit(6);
-        }
-        printf("\nUsuario: %s  ", objStore.Name);
-        printf("\nMensagem: %s\n", objStore.Msg);
-    }
-}
-
-// Procedimento para opcao 2 -  Ler mensagens 
-void encontrar_usuario_mensagens(){
-    Obj obj;
-    obj.Opcao = 2;
-    strcpy(obj.Name,"");
-    strcpy(obj.Msg,"");
-
-    int sizeOfObjStore = 0;
-
-    if (send(s, &obj, (sizeof(obj)), 0) < 0)
-    {
-        perror("Send()");
-        exit(5);
-    }
-    printf("\nMensagem enviada ao servidor - opcao 2\n");
-
-    if (recv(s, &sizeOfObjStore, sizeof(sizeOfObjStore), 0) < 0)
-    {
-        perror("Recv()");
-        exit(6);
-    }
-
-    printMessages(sizeOfObjStore);
-
-	// encontrar todos os usuarios e suas mensagens cadastradas
-};
-
-// Procedimento para opcao 3 - Apagar mensagens 
-void apagar_usuario_mensagens(){
-    Obj obj;
-    obj.Opcao = 3;
-    char name[MaxNAME];
-
-    printf("\nUsuario: \n");
-    scanf("%19s", name);
-    strcpy(obj.Name,name);
-
-
-    acessar_servidor_apagar_mensagem(obj);
-	// encontrar usuario e apagar a mensagem ! *obs : retornar a mensagem removida
-
-};
-
-
-// MAIN FUNCTION
 int main(int argc, char **argv)
 {
     unsigned short port;       
-             
+    char sendbuf[12];              
+    char recvbuf[12];              
     struct hostent *hostnm;    
     struct sockaddr_in server; 
-    int choice;
-    bool variavelLoop = true;
-
+    int s;                     
 
     /*
      * O primeiro argumento (argv[1]) e o hostname do servidor.
@@ -204,10 +48,9 @@ int main(int argc, char **argv)
     server.sin_port        = htons(port);
     server.sin_addr.s_addr = *((unsigned long *)hostnm->h_addr);
 
-
-   /*
-    * Cria um socket TCP (stream)
-    */
+    /*
+     * Cria um socket TCP (stream)
+     */
     if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("Socket()");
@@ -221,42 +64,28 @@ int main(int argc, char **argv)
         exit(4);
     }
 
+    strcpy(sendbuf, "Requisicao");
 
-    /*
-     * Criar Switch Case + Input de dados
-     */
-    do{
-    variavelLoop = false;
-    printf("\n 1 - Cadastrar mensagem \n 2 - Ler mensagens \n 3 - Apagar mensagens \n 4 - Sair da Aplicacao \n");
-    scanf("%d", &choice);
-
-
-
-    switch (choice){
-    case 1:
-	adicionar_usuario_mensagens();
-        break;
-    case 2:
-	encontrar_usuario_mensagens();
-        break;
-    case 3:
-	apagar_usuario_mensagens();
-        break;
-    case 4:
-        printf("\nFim de aplicação \n");
-	variavelLoop = true;
-
-        break;
-    default:
-	printf("\nOpcao invalida \n");
-
+    /* Envia a mensagem no buffer de envio para o servidor */
+    if (send(s, sendbuf, strlen(sendbuf)+1, 0) < 0)
+    {
+        perror("Send()");
+        exit(5);
     }
-}while(!variavelLoop);
+    printf("Mensagem enviada ao servidor: %s\n", sendbuf);
+
+    /* Recebe a mensagem do servidor no buffer de recepcao */
+    if (recv(s, recvbuf, sizeof(recvbuf), 0) < 0)
+    {
+        perror("Recv()");
+        exit(6);
+    }
+    printf("Mensagem recebida do servidor: %s\n", recvbuf);
 
     /* Fecha o socket */
     close(s);
 
-    printf("\nCliente terminou com sucesso.\n");
+    printf("Cliente terminou com sucesso.\n");
     exit(0);
 
 }
