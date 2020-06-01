80b22198305764c4d2909dfa4cb7c1447664a644
Joao Pedro Favara <joaopedro_favara@hotmail.com>
2020-06-01 19:34:11 -0300

    Cria agenda/contatos e exibie status on/off

--- Projeto 2/cliente_psta.c
+++ Projeto 2/cliente_psta.c
@@ -18,6 +18,7 @@ Kaíque Ferreira Fávero 15118698
 #include <dirent.h>
 #include <libgen.h>
 #include <sys/stat.h>
+#include <pthread.h>
 
 /*
  * Cliente TCP
@@ -27,6 +28,7 @@ Kaíque Ferreira Fávero 15118698
 #define FILESIZE 4096
 #define DIGITOSTELEFONE 9
 #define STRINGSIZE 90
+#define TAMANHOMENSAGEM 2000
 
 struct dados_cliente {
 	char telefone[DIGITOSTELEFONE];
@@ -39,15 +41,17 @@ struct agenda {
     struct agenda *prox;
 };
 
-int s;
+int s_server;
+int s_cliente , ns_cliente;
 
 //variaveis para a criacao do socket de dados
 char ip[STRINGSIZE]; // buffer temporario para string
 int s_dados, ns_dados, port_dados;
-struct sockaddr_in client_dados; 
+pthread_t thread_ServerCliente;
+struct sockaddr_in client_dados, cliente_mensagem; 
 struct sockaddr_in server_dados; 
 struct dados_cliente cliente;
-int namelen_dados;
+int namelen_dados, namelen_mensagem;
 
 char command[COMMAND];
 
@@ -61,6 +65,34 @@ long file_size(char *name) {
         fclose(fp);
     }
     return size;
+}
+
+void *thread_server() {
+    char tipo_mensagem[10];
+    while(true) {
+        /*
+        * Aceita uma conexao e cria um novo socket atraves do qual
+        * ocorrera a comunicacao com o cliente.
+        */
+        namelen_mensagem = sizeof(cliente_mensagem);
+        if ((ns_cliente = accept(s_dados, (struct sockaddr *) &cliente_mensagem, (socklen_t *) &namelen_mensagem)) == -1) {
+            perror("Accept() 1234");
+            exit(5);
+        }
+
+        if (recv(ns_cliente, &tipo_mensagem, (sizeof(tipo_mensagem)), 0) < 0) {
+            perror("Recv() xxxx ");
+            exit(6);
+        }
+
+        if ((strcmp(tipo_mensagem, "mensagem") == 0)) {
+            printf("Mensagem Recebida ...");
+        }
+        
+        if ((strcmp(tipo_mensagem, "arquivo") == 0)){
+            printf("Arquivo Recebido ...");
+        }
+	}
 }
 
 void create_socket() {
@@ -104,7 +136,7 @@ void create_socket() {
 
     /* Consulta qual porta foi utilizada. */
     namelen_dados = sizeof(server_dados);
-    if (getsockname(s_dados, (struct sockaddr *) &server_dados, &namelen_dados) < 0) {
+    if (getsockname(s_dados, (struct sockaddr *) &server_dados, (socklen_t *) &namelen_dados) < 0) {
         perror("getsockname()");
         exit(1);
     }
@@ -124,21 +156,10 @@ void create_socket() {
 	  exit(4);
     }
 
-
-    //  while(ns_dados == 0)
-    // {
-	//   /*
-	//   * Aceita uma conexao e cria um novo socket atraves do qual
-	//   * ocorrera a comunicacao com o cliente.
-	//   */
-	//   namelen_dados = sizeof(client_dados);
-	//   if ((ns_dados = accept(s_dados, (struct sockaddr *) &client_dados, (socklen_t *) &namelen_dados)) == -1) {
-	// 	perror("Accept()");
-	// 	exit(5);
-	//   }
-	// }
-
-    // return ntohs(server_dados.sin_port);
+    if (pthread_create(&thread_ServerCliente, NULL, thread_server, NULL)) {
+        printf("ERRO: impossivel criar uma thread\n");
+        exit(-1);
+    }
 }
 
 void enviar(char comando[], char nome_local[], char nome_remoto[]) {
@@ -152,20 +173,20 @@ void enviar(char comando[], char nome_local[], char nome_remoto[]) {
 
     strcpy(str_aux, comando);
     // enviar o comando enviar ...
-    if (send(s, &str_aux, (sizeof(str_aux)), 0) < 0) {
+    if (send(s_server, &str_aux, (sizeof(str_aux)), 0) < 0) {
         perror("Send() 1");
         exit(5);
     }
 
     // envia o nome que o arquivo sera salvo
     strcpy(str_aux, nome_remoto);
-    if (send(s, &str_aux, (sizeof(str_aux)), 0) < 0) {
+    if (send(s_server, &str_aux, (sizeof(str_aux)), 0) < 0) {
         perror("Send() 2");
         exit(5);
     }
 
     //enviar a porta onde sera conectado o servidor ...
-    if (send(s, &port_dados, (sizeof(port_dados)), 0) < 0)
+    if (send(s_server, &port_dados, (sizeof(port_dados)), 0) < 0)
     {
         perror("Send()");
         exit(5);
@@ -227,7 +248,7 @@ void receber(char comando[], char nome_remoto[], char nome_local[]) {
 
     strcpy(str_aux, comando);
     // enviar o comando receber ...
-    if (send(s, &str_aux, (sizeof(str_aux)), 0) < 0) {
+    if (send(s_server, &str_aux, (sizeof(str_aux)), 0) < 0) {
         perror("Send() 1");
         exit(5);
     }
@@ -235,14 +256,14 @@ void receber(char comando[], char nome_remoto[], char nome_local[]) {
     // envia o nome que o arquivo sera salvo
     strcpy(str_aux, nome_remoto);
     printf("str_aux: %s\n", str_aux);
-    if (send(s, &str_aux, (sizeof(str_aux)), 0) < 0) {
+    if (send(s_server, &str_aux, (sizeof(str_aux)), 0) < 0) {
         perror("Send() 2");
         exit(5);
     }
 
     //enviar a porta para o servidor se conectar
     printf("port: %i\n", port_dados);
-    if (send(s, &port_dados, (sizeof(port_dados)), 0) < 0)
+    if (send(s_server, &port_dados, (sizeof(port_dados)), 0) < 0)
     {
         perror("Send()");
         exit(5);
@@ -301,8 +322,9 @@ void receber(char comando[], char nome_remoto[], char nome_local[]) {
 }
 
 void encerrar() {
-    close(s);
+    close(ns_cliente);
     close(s_dados);
+    close(s_server);
     printf("Finalizando o cliente ...\n");
     exit(0);
 }
@@ -316,27 +338,40 @@ void insere_lista(char telefone[], struct agenda *a) {
 
     novo = malloc (sizeof (struct agenda));
     strcpy(novo->conteudo, telefone);
-    printf("novo->conteudo: %s\n", novo->conteudo);
+    // printf("novo->conteudo: %s\n", novo->conteudo);
 
     while (atual->prox != NULL) {
+        // printf("atual->conteudo: %s\n", atual->conteudo);
         atual = proximo;
         proximo = atual->prox;
     }
 
     if (proximo == NULL) {
+        // printf("atual->conteudo: %s\n", atual->conteudo);
         novo->prox = atual->prox;
         atual->prox = novo;
     }
 };
 
 void listar_contatos() {
+    struct controle_status {
+        char telefone[DIGITOSTELEFONE];
+        char status[10];
+    };
+
+    struct controle_status status;
     FILE *fp;
-    char nomeArquivo[20], file_name[20], line[10], status[10];
-    int countTelefone = 0;
+    char str[DIGITOSTELEFONE];
+    size_t len;
+    char nomeArquivo[20], file_name[20];
+    char *line = NULL;
+    int countTelefone = 0, read = 0;
 
     struct agenda *contato_agenda;
     contato_agenda = malloc (sizeof (struct agenda));
     contato_agenda->prox = NULL;
+    strcpy(contato_agenda->conteudo, "0000");
+
 
     strcpy(nomeArquivo, cliente.telefone);
     strcat(nomeArquivo, ".txt");
@@ -347,16 +382,37 @@ void listar_contatos() {
         printf("Nao possui contatos cadastrados ...\n");
     } else {
         fp = fopen(file_name, "r");// abre o arquivo
-        printf("TESTEETEETE ...\n");
-        while(fgets(line,1024,fp)) {
-            printf("%s\n",line);
+        // char *fcontent = malloc(size);
+        // fread(fcontent, 1, size, fp);
+        // char *pt;
+        // pt = strtok (fcontent,"");
+        // while (pt != NULL) {
+        //     printf("%s\n",pt);
+        //     insere_lista(pt, contato_agenda);
+        //     countTelefone++;
+        //     pt = strtok (NULL, ",");
+        // }
+
+        int comando = 2;
+        if (send(s_server, &comando, (sizeof(comando)), 0) < 0) {
+            perror("Send() 1");
+            exit(5);
+        }
+
+        while ((read = getline(&line, &len, fp)) != -1) {
+            strtok(line, "\n");
+            printf("> %s - %zu\n", line, len);
             insere_lista(line, contato_agenda);
             countTelefone++;
+            printf("countTelefone: %i\n", countTelefone);
         };
+
+        printf("final - countTelefone: %i\n", countTelefone);
+        printf("Retrieved line of length %d:\n", read);
 
         fclose(fp);
 
-        if (send(s, &countTelefone, (sizeof(countTelefone)), 0) < 0) {
+        if (send(s_server, &countTelefone, sizeof(countTelefone), 0) < 0) {
             perror("Send() 1");
             exit(5);
         }
@@ -364,18 +420,21 @@ void listar_contatos() {
         struct agenda *print;
         print = contato_agenda->prox;
         for(int i = 0; i < countTelefone; i++) {
+            char conteudo[DIGITOSTELEFONE];
+            strcpy(conteudo, print->conteudo);
 
-            if (send(s, &print->conteudo, (sizeof(print->conteudo)), 0) < 0) {
+            printf("print->conteudo[%i]: %s - %lu\n", i, conteudo, sizeof(conteudo));
+            if (send(s_server, conteudo, sizeof(conteudo), 0) < 0) {
                 perror("Send() 1");
                 exit(5);
             }
 
-            if (recv(s, &status, (sizeof(status)), 0) < 0) {
+            if (recv(s_server, &status, sizeof(status), 0) < 0) {
                 perror("Recv()");
                 exit(6);
             }
 
-            printf("%i) %s - %s\n", i+1, print->conteudo, status);
+            printf("%i) %s - %s\n", i+1, status.telefone, status.status);
 
             print = print->prox;
         }
@@ -394,7 +453,7 @@ void listar(const char list_command[]) {
 
     //envia o comando "listar"
     strcpy(comando_listar, list_command);
-    if (send(s, &comando_listar, (sizeof(comando_listar)), 0) < 0)
+    if (send(s_server, &comando_listar, (sizeof(comando_listar)), 0) < 0)
     {
         perror("Send()");
         exit(5);
@@ -402,7 +461,7 @@ void listar(const char list_command[]) {
 
     printf("port: %i\n", port_dados);
     // enviar para o servidor a porta q ocorerra a comunicacao com o servidor
-    if (send(s, &port_dados, (sizeof(port_dados)), 0) < 0)
+    if (send(s_server, &port_dados, (sizeof(port_dados)), 0) < 0)
     {
         perror("Send()");
         exit(5);
@@ -440,13 +499,13 @@ void listar(const char list_command[]) {
     ns_dados = 0;
 }
 
-void conectar(char hostname[], char porta[]) {
+void conectar(char hostname[], char porta[], int *s) {
     unsigned short port;
     struct hostent *hostnm;
     struct sockaddr_in server;
 
-    // printf("hostname: %s\n", hostname);
-    // printf("porta: %s\n", porta);
+    printf("hostname: %s\n", hostname);
+    printf("porta: %s\n", porta);
 
     /*
      * Obtendo o endereco IP do servidor
@@ -466,23 +525,68 @@ void conectar(char hostname[], char porta[]) {
     server.sin_port        = htons(port);
     server.sin_addr.s_addr = *((unsigned long *)hostnm->h_addr);
 
-    inet_ntop(AF_INET,&server.sin_addr,ip,STRINGSIZE);
+    inet_ntop(AF_INET, &server.sin_addr, ip, STRINGSIZE);
     printf("server.sin_addr.s_addr: %s\n", ip);
 
     /*
      * Cria um socket TCP (stream)
      */
-    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    if ((*s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("Socket()");
         exit(3);
     }
 
     /* Estabelece conexao com o servidor */
-    if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+    if (connect(*s, (struct sockaddr *)&server, sizeof(server)) < 0)
     {
         perror("Connect()");
         exit(4);
+    }
+}
+
+void enviar_mensagem() {
+    char telefone_mensagem[DIGITOSTELEFONE];
+    struct dados_cliente dados;
+    char mensagem[TAMANHOMENSAGEM];
+    char porta[32];
+    char tipo_mensagem[20] = "mensagem";
+
+    printf("Digite o telefone para enviar a mensagem:\n");
+    fpurge(stdin);
+    fgets(telefone_mensagem,sizeof(telefone_mensagem),stdin);
+    strtok(telefone_mensagem, "\n");
+
+    if (send(s_server, &telefone_mensagem, (sizeof(telefone_mensagem)), 0) < 0) {
+        perror("Send() 1");
+        exit(5);
+    }
+
+    if (recv(s_server, &dados, (sizeof(dados)), 0) < 0) {
+        perror("Recv()");
+        exit(6);
+    }
+
+    printf("Telefone: %s\n", dados.telefone);
+    printf("Porta: %i\n", dados.porta);
+    printf("IP: %s\n", dados.ip);
+
+    if (dados.porta == 0) {
+        printf("Contato offline ... Tente mais tarde\n");
+    } else {
+        printf("Digite a mesnagem:\n");
+        fpurge(stdin);
+        fgets(mensagem,sizeof(mensagem),stdin);
+        sprintf(porta, "%i", dados.porta);
+
+        conectar(dados.ip, porta, &s_cliente);
+
+        if (send(s_cliente, &tipo_mensagem, (sizeof(tipo_mensagem)), 0) < 0) {
+            perror("Send() 1");
+            exit(5);
+        }
+
+        printf("Texto ... TEL: %s\nMENSAGEM: %s\n", telefone_mensagem, mensagem);
     }
 }
 
@@ -496,17 +600,95 @@ void adicionar_contato() {
 
     fpurge(stdin);
     fgets(telefone,sizeof(telefone),stdin);
+    strtok(telefone, "\n");
 
     strcpy(nomeArquivo, cliente.telefone);
     strcat(nomeArquivo, ".txt");
     snprintf(file_name, (strlen(nomeArquivo) + 1), "%s", nomeArquivo);// pega o nome do arquivo de um uma variavel
     fp = fopen(file_name, "a");// abre o arquivo
 
-    fprintf(fp, "%s", telefone);
+    fprintf(fp, "%s\n", telefone);
 	fclose(fp);
 
     printf("Novo contato salvo com sucesso!\n");
 };
+
+void menu_chat_tipo_mensagem() {
+    bool variavelLoop = false;
+    int comando;
+
+    do {
+        printf("CHAT - Selecione o tipo de mensagem:\n"
+            "1 - Texto\n"
+            "2 - Arquivo\n"
+            "3 - Sair do Chat\n\n"
+            "Digite o numero da opcao: ");
+
+        fpurge(stdin);
+        scanf("%i", &comando);
+
+        switch (comando) {
+        case 1:
+            comando = 3; // para acessar a funcao 3 do servidor
+            if (send(s_server, &comando, (sizeof(comando)), 0) < 0) {
+                perror("Send() 1");
+                exit(5);
+            }
+            enviar_mensagem();
+            break;
+
+        case 2:
+            printf("Arquivo ...");
+            break;
+
+        case 3:
+            variavelLoop = true;
+            break;
+
+        default:
+            printf("Comando invalido ... Por favor insira um novo comando\n");
+            break;
+        }
+
+    } while(!variavelLoop);
+
+}
+
+void menu_chat() {
+    bool variavelLoop = false;
+    int comando;
+
+    do {
+        printf("CHAT - Enviar mensagem para:\n"
+            "1 - Individual\n"
+            "2 - Grupo\n"
+            "3 - Sair do Chat\n\n"
+            "Digite o numero da opcao: ");
+
+        fpurge(stdin);
+        scanf("%i", &comando);
+
+        switch (comando) {
+        case 1:
+            menu_chat_tipo_mensagem();
+            break;
+
+        case 2:
+            printf("GRUPO ...");
+            break;
+
+        case 3:
+            variavelLoop = true;
+            break;
+
+        default:
+            printf("Comando invalido ... Por favor insira um novo comando\n");
+            break;
+        }
+
+    } while(!variavelLoop);
+
+}
 
 void criar_contato() {
     char telefone[DIGITOSTELEFONE];
@@ -516,6 +698,7 @@ void criar_contato() {
 
     fpurge(stdin);
     fgets(telefone,sizeof(telefone),stdin);
+    strtok(telefone, "\n");
 
     cliente.porta = port_dados;
     strcpy(cliente.telefone, telefone);
@@ -526,7 +709,7 @@ void criar_contato() {
 	printf("IP: %s\n", cliente.ip);
 
     // enviar para o servidor a porta do socket de dados e do telefone
-    if (send(s, &cliente, (sizeof(cliente)), 0) < 0) {
+    if (send(s_server, &cliente, (sizeof(cliente)), 0) < 0) {
         perror("Send() 3");
         exit(5);
     }
@@ -536,14 +719,12 @@ void criar_contato() {
 
 // MAIN FUNCTION
 int main(int argc, char **argv){
-    conectar(argv[1], argv[2]);
+    conectar(argv[1], argv[2], &s_server);
     criar_contato();
-    bool variavelLoop = true;
+    bool variavelLoop = false;
     int comando;
 
     do {
-
-        variavelLoop = false;
         printf("MENU:\n"
             "1 - Adicionar novo contato\n"
             "2 - Ver contatos\n"
@@ -551,34 +732,24 @@ int main(int argc, char **argv){
             "4 - Sair da aplicacao\n\n"
             "Digite o numero da opcao: ");
 
-        // memset(comando, 0, sizeof(comando));
         fpurge(stdin);
         scanf("%i", &comando);
 
         switch (comando) {
         case 1:
-            // if (send(s, &comando, (sizeof(comando)), 0) < 0) {
-            //     perror("Send() 1");
-            //     exit(5);
-            // }
-
             adicionar_contato();
             break;
 
         case 2:
-            if (send(s, &comando, (sizeof(comando)), 0) < 0) {
-                perror("Send() 1");
-                exit(5);
-            }
             listar_contatos();
             break;
         
         case 3:
-            printf("Comando 3 ...\n");
+            menu_chat();
             break;
 
         case 4:
-            if (send(s, &comando, (sizeof(comando)), 0) < 0) {
+            if (send(s_server, &comando, (sizeof(comando)), 0) < 0) {
                 perror("Send()");
                 exit(5);
             }
@@ -594,7 +765,7 @@ int main(int argc, char **argv){
     } while(!variavelLoop);
 
     /* Fecha o socket */
-    close(s);
+    close(s_server);
     printf("Cliente terminou com sucesso.\n");
     exit(0);
 }
